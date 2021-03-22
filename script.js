6763462edd63ecbc8923bb43598be76c1f6813e5
TheFlorin97 <constantin.florin97@yahoo.com>
2021-03-22 19:32:22 +0200

    btn - switch

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 var myStyle;
 
 myStyle = document.querySelector('.myStyle');
+document.querySelector(".btn2").style.display = 'none';
 
 
-
-document.querySelector(".btn__style").addEventListener('click', function () {
-
-
+document.querySelector(".btn1").addEventListener('click', function () {
     myStyle.setAttribute('href', "style2.css");
-    console.log(myStyle.href);
+    document.querySelector(".btn2").style.display = 'block';
+    document.querySelector(".btn1").style.display = 'none';
+
+});
+
+document.querySelector(".btn2").addEventListener('click', function () {
+    myStyle.setAttribute('href', "style1.css");
+    document.querySelector(".btn1").style.display = 'block';
+    document.querySelector(".btn2").style.display = 'none';
 
 });
 
