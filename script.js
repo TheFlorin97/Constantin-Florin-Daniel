96ca9a1546199cea2e42cbbdcf4240b4d943928c
TheFlorin97 <constantin.florin97@yahoo.com>
2021-03-22 19:04:07 +0200

    Link

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var bodyE = document.getElementsByClassName('par');
-var text = "<p>Ala Bala</p>"
-bodyE.innerHTML = text;
+var myStyle;
+
+myStyle = document.querySelector('.myStyle');
+
+
+
+document.querySelector(".btn__style").addEventListener('click', function () {
+
+
+    myStyle.setAttribute('href', "style2.css");
+    console.log(myStyle.href);
+
+});
+
